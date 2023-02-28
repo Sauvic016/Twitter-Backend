1891c2874d13b89bbf9901bf14287082a3ff9a65
Sauvic <sauvicpaulchy@gmail.com>
2023-02-28 19:17:43 +0530

    feat: Add like model and basic user model

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -4,7 +4,6 @@ const tweetService = new TweetService();
 
 export const createTweet = async (req, res) => {
   try {
-    console.log(req.body);
     const response = await tweetService.create(req.body);
     return res.status(201).json({
       success: true,
@@ -13,7 +12,6 @@ export const createTweet = async (req, res) => {
       err: {},
     });
   } catch (error) {
-    console.log(error);
     return res.status(500).json({
       success: false,
       data: {},
