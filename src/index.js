1c3afd77df3933d7e551fc1933492795b7717798
Sauvic <sauvicpaulchy@gmail.com>
2023-03-01 13:38:23 +0530

    feat: Add api to toggle likes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import express from "express";
 
 import connect from "./config/database.js";
 import apiRoutes from "./routes/index.js";
-import { UserRepository, TweetRepository } from "./repository/index.js";
-import LikeService from "./services/like-service.js";
 
 const app = express();
 
@@ -16,15 +14,4 @@ app.listen(3000, async () => {
   console.log("Server started at Port", 3000);
   await connect();
   console.log("Mongo db connected");
-
-  const userRepo = new UserRepository();
-  const tweetRepo = new TweetRepository();
-  const tweets = await tweetRepo.getAll(0, 10);
-  // const user = await userRepo.create({
-  //   email: "Sauvic@gmail.com",
-  //   password: "123456",
-  //   name: "Sauvic",
-  // });
-  const likeService = new LikeService();
-  await likeService.toggleLike(tweets[0].id, "Tweet", "63fe42487900095947ae09e9");
 });
