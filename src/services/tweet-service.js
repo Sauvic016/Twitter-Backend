4008b410a71babb22b6939eafdbcfe0bd80572c4
Sauvic <sauvicpaulchy@gmail.com>
2023-03-06 23:09:23 +0530

    fix: Fixes bug in creating tweet with no tags

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -8,30 +8,31 @@ class TweetService {
   async create(data) {
     const content = data.content;
     let tags = content.match(/#[a-zA-Z0-9_]+/g); // this regex extracts hashtags
+    if (tags) {
+      tags = tags.map((tag) => tag.substring(1).toLowerCase());
+      let tagSet = new Set(tags);
+      tags = Array.from(tagSet);
+      const tweet = await this.tweetRepository.create(data);
 
-    tags = tags.map((tag) => tag.substring(1).toLowerCase());
-    let tagSet = new Set(tags);
-    tags = Array.from(tagSet);
+      let existingHashtags = await this.hashtagRepository.findByName(tags);
+      let titleOfPresentTags = existingHashtags.map((tag) => tag.title);
+
+      let newHashtags = tags.filter((tag) => {
+        return !titleOfPresentTags.includes(tag);
+      });
+
+      newHashtags = newHashtags.map((tag) => {
+        return { title: tag, tweets: [tweet.id] };
+      });
+
+      await this.hashtagRepository.bulkCreate(newHashtags);
+
+      existingHashtags.forEach((tag) => {
+        tag.tweets.push(tweet.id);
+        tag.save();
+      });
+    }
     const tweet = await this.tweetRepository.create(data);
-
-    let existingHashtags = await this.hashtagRepository.findByName(tags);
-    let titleOfPresentTags = existingHashtags.map((tag) => tag.title);
-
-    let newHashtags = tags.filter((tag) => {
-      return !titleOfPresentTags.includes(tag);
-    });
-
-    newHashtags = newHashtags.map((tag) => {
-      return { title: tag, tweets: [tweet.id] };
-    });
-
-    await this.hashtagRepository.bulkCreate(newHashtags);
-
-    existingHashtags.forEach((tag) => {
-      tag.tweets.push(tweet.id);
-      tag.save();
-    });
-
     return tweet;
   }
 
