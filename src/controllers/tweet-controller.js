112685f773aeb0b9b9a97dee06814f43a5b43807
Sauvic <sauvicpaulchy@gmail.com>
2023-03-06 23:23:59 +0530

    feat: Add logic to upload multiple image

--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -2,20 +2,22 @@ import TweetService from "../services/tweet-service.js";
 import upload from "../config/file-upload-s3-config.js";
 
 const singleUploader = upload.single("image");
+const multipleUpload = upload.array("images", 8);
 
 const tweetService = new TweetService();
 
 export const createTweet = async (req, res) => {
   try {
-    singleUploader(req, res, async function (err, data) {
+    multipleUpload(req, res, async function (err, data) {
       if (err) {
         return res.status(500).json({
           error: err,
         });
       }
-      console.log("Image url is ", req.file);
       const payload = { ...req.body };
-      payload.image = req.file.location;
+      if (req.files && req.files.length) {
+        payload.images = req.files.map((file) => file.location);
+      }
       const response = await tweetService.create(payload);
       return res.status(201).json({
         success: true,
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -19,9 +19,11 @@ const tweetSchema = new mongoose.Schema(
         ref: "Comment",
       },
     ],
-    image: {
-      type: String,
-    },
+    images: [
+      {
+        type: String,
+      },
+    ],
   },
   { timestamps: true }
 );
